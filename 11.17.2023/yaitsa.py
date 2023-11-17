20b4d061b931d4e06ea5f4ceafa29dccfbee8e5f
Xeon_Fox <semyon.24.04.2008@gmail.com>
2023-11-17 21:34:06 +0600

    Create yaitsa.py

---
+++ 11.17.2023/yaitsa.py
@@ -0,0 +1,55 @@
+import math
+#1 firstnumberverycoolamazing=float(input("3 chsla vvedi 1 "))
+# secondnumberverycoolamazing=float(input("3 chsla vvedi 2 "))
+# thirdnumberverycoolamazing=float(input("3 chsla vvedi 3 "))
+# maxthing = max(firstnumberverycoolamazing, secondnumberverycoolamazing, thirdnumberverycoolamazing, key=abs)
+# print("max==", maxthing)
+
+#2 bukvarusskovoorenglishalphabet=input("BURVU NAPSHI EST JE: ")
+# sledbukvavelikovorusskvoorenglishsimbol=ord(bukvarusskovoorenglishalphabet) + 1
+# sledbukvavelikovorusskvoorenglishtext=chr(sledbukvavelikovorusskvoorenglishsimbol)
+# print(sledbukvavelikovorusskvoorenglishtext)
+
+#3 temperatureofvozdyhaday=float(input("temperature day vvedi(╯°□°）╯︵ ┻━┻(╯°□°）╯︵ ┻━┻(╯°□°）╯︵ ┻━┻ : "))
+# temperatureofvozdyhanoch=float(input("temperature ночь vvedi(ಥ_ಥ): "))
+# velichinachototamyanesharuvgeorgaphii=temperatureofvozdyhaday-temperatureofvozdyhanoch
+# velichinachototamyanesharuvgeorgaphii = abs(velichinachototamyanesharuvgeorgaphii)
+# print(velichinachototamyanesharuvgeorgaphii, " градусов по чему вы там ввели")
+
+#4 drobnoechisloottupogousera=float(input("огурец🥒: "))
+# skokatamznakovnada=int(input("точка запятая зернадцать: "))
+# resultofthisshit=round(drobnoechisloottupogousera, ndigits=skokatamznakovnada)
+# print(resultofthisshit, "ваше число, жду чаевых")
+
+#5 chislo_zachemto_isportit_kotoroe=int(input("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.: "))
+# binarchisloperemenaoguzok=bin(chislozachemtoisportitkotoroe)
+# vosemchislodropdesateptvoiu=oct(chislozachemtoisportitkotoroe)
+# desat=hex(chislozachemtoisportitkotoroe)
+# print('1 ', binarchisloperemenaoguzok, '2 ', vosemchislodropdesateptvoiu, '3 ', desat)
+
+#6 uskoreneefizikagdz7klass=float(input("a: "))
+# velocityoffiziksinthetask=float(input("v= "))
+# time_vremia_t_shitjopa=float(input("t являеться чем "))
+# s=(uskoreneefizikagdz7klass*time_vremia_t_shitjopa**2)/2 + velocityoffiziksinthetask * time_vremia_t_shitjopa
+# print("s=", s)
+
+#7 x1_coordinate_algebra_10klass_gdz=float(input("x1 оордината "))
+# y1_absis_piska_jopa=float(input("y1 координата абцис "))
+# x2_coordinate_algebra_10klass_gdz=float(input("x2 оордината "))
+# y2_absis_piska_jopa=float(input("y2 координата абцис "))
+# rasstoyanie=math.sqrt((x2_coordinate_algebra_10klass_gdz-x1_coordinate_algebra_10klass_gdz)**2+(y2_absis_piska_jopa-y1_absis_piska_jopa)**2)
+# print(rasstoyanie)
+
+#8 firstchetotameptvoyou=float(input("asdasd "))
+# secondshitpoststatus=float(input("asdaszxc "))
+# x=-(secondshitpoststatus/firstchetotameptvoyou)
+# print(x)
+
+#9 
+hours_current_time_world_usa_kosovo=int(input("chas= "))
+minutes_current_time_world_usa_kosovo=int(input("minutes= "))
+dayintheworldhowmuch=23
+minuteshowmuchisit= 60
+dayhours=dayintheworldhowmuch-hours_current_time_world_usa_kosovo
+minutesshit=minuteshowmuchisit-minutes_current_time_world_usa_kosovo
+print(dayhours," часов ", minutesshit, " минут")
