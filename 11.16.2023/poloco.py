cad41a8673e9bd6bf67117a90e0f578052618970
Xeon_Fox <semyon.24.04.2008@gmail.com>
2023-11-17 18:53:40 +0600

    Create poloco.py

---
+++ 11.16.2023/poloco.py
@@ -0,0 +1,32 @@
+#1: r=input("kopa: ")
+# sa = int(r)
+# saz = sa ** 2
+# print(saz)
+
+#2: a = float(input("pisa1: "))
+# b = float(input("pisa2: "))
+# c = (a+b)/2
+# print(c)
+
+#3: ploshadInputOnlineInPython=float(input("ВВДЕТИЕ ПЛОЩАД ПЖ🌹🙌🙌🙌: "))
+# ploshadoOutputOnlineInPython= ploshadInputOnlineInPython*ploshadInputOnlineInPython #данное действие выполняет невероятную функции содержащию в себя сочетание двух сторон квадрата выдающей значение общей площади планиметрической фигуры под название "квадрат" квадрат имеет 4 общих равных стороны: это отлечительный знак квадратов. Также, у квадратов все уголы равны 90 градусов. Площадь квадрата это общая территория находящиеся внутри данной фигуры. Квадраты одна из самых важных честей планиметрии. Формулы S=a*a проходят в 3 классе на уроках математики. Хоть она кажеться и простой из нее выводят огромное колличество формул.
+# print(ploshadoOutputOnlineInPython)
+
+#4: KilometersVvednniChelovekom=float(input("не позволите ли вы вписать километы "))
+# NatoAmericanMileIhateamerica=float
+# NatoAmericanMileIhateamerica=0.62
+# kmmileburgeramericailoveamerica=KilometersVvednniChelovekom*NatoAmericanMileIhateamerica
+# print(kmmileburgeramericailoveamerica)
+
+#5: firstnumberepicbattlecalculator=float(input("еен "))
+# secondnumberepicbattlecalculator=float(input("леен2 "))
+# pluspppp=firstnumberepicbattlecalculator+secondnumberepicbattlecalculator
+# minsuyas=firstnumberepicbattlecalculator-secondnumberepicbattlecalculator
+# umnozit=firstnumberepicbattlecalculator*secondnumberepicbattlecalculator
+# delit=firstnumberepicbattlecalculator/secondnumberepicbattlecalculator
+# print(pluspppp," ",minsuyas," ",umnozit," ", delit)
+
+firstchetotameptvoyou=float(input("asdasd "))
+secondshitpoststatus=float(input("asdaszxc "))
+x=-(secondshitpoststatus/firstchetotameptvoyou)
+print(x)
