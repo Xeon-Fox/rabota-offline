e50fdcf6effad2a8305aaa7508a33c559dd480c3
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2023-11-24 18:46:53 +0600

    Update z.py

--- 11.23.2023/z.py
+++ 11.23.2023/z.py
@@ -1,33 +1,20 @@
 hz_che=input("llas: ")
-try:
-    month_mesac_ai=int(hz_che)
-except ValueError:
-    print("число сделай")
-else:
-    match month_mesac_ai:
-        case 1:
-            print("джануари")
-        case 2:
-            print("фебруари")
-        case 3:
-            print("марч")
-        case 4:
-            print("эйприл")
-        case 5:
-            print("мэй")
-        case 6:
-            print("джун")
-        case 7:
-            print("джулай")
-        case 8:
-            print("оугуст")
-        case 9:
-            print("сэптэмбэр")
-        case 10:
-            print("октобэр")
-        case 11:
-            print("новэмбер")
-        case 12:
-            print("десэмбэр")
-        case _:
-            print("эт че")
+message_whatsapp_viber_Skype=""
+match hz_che:
+    case "понедельник":
+        message_whatsapp_viber_Skype="Понедельник-день-бездельник"
+    case "вторник":
+        message_whatsapp_viber_Skype="12-день-бездельник"
+    case "среда":
+        message_whatsapp_viber_Skype="1212-день-бездельник"
+    case "четверг":
+        message_whatsapp_viber_Skype="Понедель1112ник-день-бездельник"
+    case "пятница":
+        message_whatsapp_viber_Skype="Понедель12122112ник-день-бездельник"
+    case "суббота":
+        message_whatsapp_viber_Skype="Понеде1121212льник-день-бездельник"
+    case "воскресенье":
+        message_whatsapp_viber_Skype="Понедел12122112ьник-день-бездельник"
+    case _:
+        amessage_whatsapp_viber_Skype="эт че"
+print(message_whatsapp_viber_Skype)
