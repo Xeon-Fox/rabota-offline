e37b1b92070f4009594d62a23bb17dbc711e517d
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2023-11-24 21:47:04 +0600

    asd

---
+++ 11.24.2023/desomorphine.py
@@ -0,0 +1,144 @@
+#1 walter_hartwell_white=input("CHISLOOOO ")
+# try:
+#     walter_hartwell_white = int(walter_hartwell_white)
+# except ValueError:
+#     print("втф")
+# else:
+#     if walter_hartwell_white > 0:
+#         print("больше да да")
+#     elif walter_hartwell_white < 0:
+#         print("меньше😂😁")
+#     else:
+#         print("0")
+    
+#2 age_vozrast_jaz=input("let skoka te ")
+# try:
+#     age_vozrast_jaz = int(age_vozrast_jaz)
+# except ValueError:
+#     print("бро...")
+# else:
+#     if age_vozrast_jaz<=120 and age_vozrast_jaz > 6:
+#         print("воу братан крута крутяк")
+#     elif age_vozrast_jaz>120:
+#         print("поверил👍")
+#     if age_vozrast_jaz>=0 and age_vozrast_jaz<=6:
+#         print("угугага")
+#     elif age_vozrast_jaz <= 0:
+#         print("бро тебе не минус лет")
+
+#4 chaz=input("chas nada ")
+# minutew=input("dadada ")
+# secc=input("asad ")
+# try:
+#     chaz=int(chaz)
+#     minutew=int(minutew)
+#     secc=int(secc)
+# except ValueError:
+#     print("АААААААААААААААА")
+# else:
+#     if chaz < 24:
+#         print("все все нормасик")
+#     elif chaz >= 24:
+#         print("неправильно у тебя часы идут чето")
+#     elif chaz < 0:
+#         print("минус часы??")
+#     if minutew < 60:
+#         print("минутки доже все крутяк бро")
+#     elif minutew >= 60:
+#         print("минуты не")
+#     elif minutew < 0:
+#         print("минус мин")
+#     if secc < 60:
+#         print("все все нормас время у тя")
+#     elif secc >= 60:
+#         print("a")
+#     elif secc < 0:
+#         print("минус минуты??")
+
+#5 x_algebra_10_klass=input("x absis ")
+# y_algebra_10_klass=input("y absis ")
+# try:
+#     x_algebra_10_klass=float(x_algebra_10_klass)
+#     y_algebra_10_klass=float(y_algebra_10_klass)
+# except ValueError:
+#     print(",asdm ")
+# else:
+#     if x_algebra_10_klass < 1 and y_algebra_10_klass < 1 and x_algebra_10_klass > 0 and y_algebra_10_klass > 0:
+#         print("1")
+#     elif x_algebra_10_klass < 0 and y_algebra_10_klass < 1 and x_algebra_10_klass > -1 and y_algebra_10_klass > 0:
+#         print("2")
+#     elif x_algebra_10_klass < -1 and y_algebra_10_klass < 0 and x_algebra_10_klass > 0 and y_algebra_10_klass > -1:
+#         print("3")
+#     elif x_algebra_10_klass < 1 and y_algebra_10_klass < 0 and x_algebra_10_klass > 0 and y_algebra_10_klass > -1:
+#         print("4")
+#     if x_algebra_10_klass == 1 or x_algebra_10_klass == 0 or x_algebra_10_klass == -1 and y_algebra_10_klass == 1 or y_algebra_10_klass == 0 or y_algebra_10_klass == -1:
+#         print("начало координат")
+
+#1 hz_che=input("llas: ")
+# try:
+#     hz_che=int(hz_che)
+# except ValueError:
+#     print("1asd")
+# else:
+#     message_whatsapp_viber_Skype=""
+#     match hz_che:
+#         case 1:
+#             message_whatsapp_viber_Skype="январь"
+#         case 2:
+#             message_whatsapp_viber_Skype="фев"
+#         case 3:
+#             message_whatsapp_viber_Skype="мар"
+#         case 4:
+#             message_whatsapp_viber_Skype="апр"
+#         case 5:
+#             message_whatsapp_viber_Skype="май"
+#         case 6:
+#             message_whatsapp_viber_Skype="июн"
+#         case 7:
+#             message_whatsapp_viber_Skype="июл"
+#         case 8:
+#             message_whatsapp_viber_Skype="авг"
+#         case 9:
+#             message_whatsapp_viber_Skype="сен"
+#         case 10:
+#             message_whatsapp_viber_Skype="окт"
+#         case 11:
+#             message_whatsapp_viber_Skype="ноя"
+#         case 12:
+#             message_whatsapp_viber_Skype="дек"      
+#         case _:
+#             amessage_whatsapp_viber_Skype="эт че"
+#     print(message_whatsapp_viber_Skype)
+
+#2 bir=input("1num ")
+# eki=input("2num ")
+# iio=input("znak ")
+# try:
+#     bir=int(bir)
+#     eki=int(eki)
+# except ValueError:
+#     print("asd")
+# else:
+#     match iio:
+#         case "+":
+#             print(bir+eki)
+#         case "-":
+#             print(bir-eki)
+#         case "*":
+#             print(bir*eki)
+#         case "/":
+#             print(bir/eki)
+#         case _:
+#             print("что")
+
+#1 first_nomber=input("123 ")
+# secod_nomber=input("123f ")
+# x=first_nomber if first_nomber>secod_nomber else secod_nomber
+
+nuber_otuser=input("w ")
+try:
+    nuber_otuser=int(nuber_otuser)
+except ValueError:
+    print("asd")
+else:
+    x=nuber_otuser
---
+++ 11.24.2023/methamphetamine.py
@@ -0,0 +1,45 @@
+# km_miles_cowboy_america=1.68436583
+# vvod=input("kjasd: ")
+
+# try:
+#     kingmonkey = float(vvod)
+# except ValueError:
+#     print("🤔🤔🤔")
+# else:
+#     if kingmonkey>= 0:
+#         dist = kingmonkey / km_miles_cowboy_america
+#         print(dist, " американских миль🌵🏜🔫🤠🐎")
+#     else:
+#         print("как растояние отрицательное втф")
+
+# if cost_democracy_type_anarchy < 1000:
+#     print("не жирно ли скидку на это спрашивать?")
+# elif cost_democracy_type_anarchy<2000:
+#     print("2%, жмоб")
+# elif cost_democracy_type_anarchy<5000:
+#     print("5%, рыночек зарешал")
+# else:
+#     print("10%, подавись")
+
+# cost_democracy_type_anarchy=6500
+# match cost_democracy_type_anarchy:
+#     case x if x <1000:
+#         print("не жирно ли скидку на это спрашивать?")
+#     case x if x < 2000:
+#         print("2%, жмоб")
+#     case x if x < 5000:
+#         print("5%, рыночек зарешал")
+#     case _:
+#         print("10%, подавись")
+
+# insert=input("ZV Я ZА СVО ")
+# b=10
+# try:
+#     num = int(insert)
+# except ValueError:
+#     print("jopa")
+# else:
+#     c = b*2 if num % 2 == 0 else b/2
+#     print(c)
+
+
