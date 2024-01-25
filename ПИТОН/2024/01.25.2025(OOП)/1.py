4e1e77ad0db3662fbbc1db2c8f28595e0d987ebc
Xeon_Fox <78652129+Xeon-Fox@users.noreply.github.com>
2024-01-25 21:45:06 +0600

    f

---
+++ ПИТОН/2024/01.25.2025(OOП)/1.py
@@ -0,0 +1,13 @@
+class Kettle:
+    material = "steel"
+    color = "red"
+    volume = 2.4
+    water = 0
+
+    def fill(self, liter):
+        self.water += liter
+my_kettle1 = Kettle()
+my_kettle2 = Kettle()
+print(my_kettle1.water, my_kettle2.water)
+my_kettle1.fill(2)
+print(my_kettle1.water, my_kettle2.water)
